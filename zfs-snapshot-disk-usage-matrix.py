7983cf40a43e034e8e8b47ad3a95bc16778f6dbf
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2020-11-28 14:47:04 +0100

    Label output as being a binary prefix
    This avoids confusion. See https://en.wikipedia.org/wiki/Binary_prefix

--- zfs-snapshot-disk-usage-matrix.py
+++ zfs-snapshot-disk-usage-matrix.py
@@ -44,7 +44,7 @@ from os.path import commonprefix
 def convert_size(size_bytes):
     if size_bytes == 0:
         return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    size_name = ("B", "kiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
     i = int(math.floor(math.log(size_bytes, 1024)))
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
